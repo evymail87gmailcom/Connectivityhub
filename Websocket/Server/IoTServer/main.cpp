28cecd33fe4cf892af3032ebc2b9870682d2fc00
Evy Svensson <evy.svensson@stud.sti.se>
2021-01-14 17:09:57 +0100

    Update main.cpp

--- Websocket/Server/IoTServer/main.cpp
+++ Websocket/Server/IoTServer/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, const char** argv) {
 	string url(argc >= 2 ? argv[1] : "insert_sql_url");
 	const string user(argc >= 3 ? argv[2] : "insert_sql_username");
 	const string pass(argc >= 4 ? argv[3] : "insert_password_to_sql_database_here");
-	const string database(argc >= 5 ? argv[4] : "environmentdata");
+	const string database(argc >= 5 ? argv[4] : "innodb");
 
 	//Send the incoming devicevalues to a database in mySql
 	DeviceHandler* myHandler = new DeviceHandler;
